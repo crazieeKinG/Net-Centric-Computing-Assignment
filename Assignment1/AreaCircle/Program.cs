b7d7bbcc9768fee6a0155547ae460fc66b0b1fd9
Saajan Shrestha <shresthasaajan31@gmail.com>
2020-09-12 12:22:21 +0545

    First upload

---
+++ Assignment1/AreaCircle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace AreaCircle
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Area of circle calculation... ");
+            Console.Write("Enter radius (m) : ");
+            double r = double.Parse(Console.ReadLine());
+            double a = Math.PI * r * r;
+            Console.WriteLine($"Area = {a} m^2");
+        }
+    }
+}
